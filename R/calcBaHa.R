63c17090d4f391a49f7393e533749117e92eb5f0
Lennart Noordermeer <lennart.noordermeer@nmbu.no>
2024-09-30 13:56:36 +0200

    .

--- R/calcBaHa.R
+++ R/calcBaHa.R
@@ -2,7 +2,7 @@
 #'
 #' Computes the basal area in each plot, scaled to per ha values.
 #'
-#' @param trees Data frame with columns plotID and ba (basal area of each tree in m2).
+#' @param trees Data frame with columns: plotID and ba (basal area of each tree in m2).
 #' @param plotArea Numeric value representing the plot area in m2.
 #' @return Data frame with plotID and total basal area (in m2 ha-1).
 #' @examples
