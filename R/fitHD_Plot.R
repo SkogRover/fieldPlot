fcff2eed4485b44228ca22f5e181f7e3ecc64008
lennartnoordermeer <lennart.noordermeer@nmbu.no>
2024-11-22 14:49:46 +0100

    Resolve merge conflict in fitHD_Plot.R

--- R/fitHD_Plot.R
+++ R/fitHD_Plot.R
@@ -15,7 +15,6 @@ fitHD_Plot <- function(d, h, sp, plotID) {
   }
   require(nlme)
 
-  # Create data frame and ensure proper types
   data <- data.frame(
     d = as.numeric(d),
     h = as.numeric(h),
@@ -23,28 +22,16 @@ fitHD_Plot <- function(d, h, sp, plotID) {
     plotID = as.factor(plotID)
   )
 
-  # Filter rows where height and diameter are measured
   data1 <- data[!is.na(data$h * data$d), ]
 
-  # Starting values
   start <- c(a = 1.5, b = 0.34)
 
-  # Fit nlme model
   nlme::nlme(h ~ 1.3 + (d / (a + b * d))^3,
              data = data1,
              start = start,
              fixed = list(as.formula("a~1"), as.formula("b~1")),
              random = list(plotID = list(a ~ 1)),
              control = nlmeControl(tol = 1e-6, maxIter = 100))
-
-  data1 <- trees[!is.na(trees$h * trees$dbh), ] # trees with measured height
-  start <- c(1.5, 0.34) # start values
-  nlme::nlme(h ~ 1.3 + (dbh / (a + b * dbh))^3,
-       data = data1,
-       start = start,
-       fixed = list(as.formula("a~1"), as.formula("b~1")),
-       random = list(plotID = list(a ~ 1)),
-       control = nlmeControl(tol = 1e-6, maxIter = 100))
 
 }
 
